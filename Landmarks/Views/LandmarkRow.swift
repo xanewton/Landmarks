068e1ea759879b590d9b348b47e68aaf711684a7
Angel Newton <xanewton@gmail.com>
2021-01-10 16:14:55 -0500

    Add LandmarkRow to the Views group

---
+++ Landmarks/Views/LandmarkRow.swift
@@ -0,0 +1,20 @@
+//
+//  LandmarkRow.swift
+//  Landmarks
+//
+//  Created by newtan on 2021-01-10.
+//
+
+import SwiftUI
+
+struct LandmarkRow: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct LandmarkRow_Previews: PreviewProvider {
+    static var previews: some View {
+        LandmarkRow()
+    }
+}
