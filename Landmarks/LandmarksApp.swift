65545d04bc9734d3402b85603b0be53a57c93dfb
Angel Newton <xanewton@gmail.com>
2021-01-10 15:02:38 -0500

    Tutorial 1 - Creating and Combining Views

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by newtan on 2021-01-09.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
